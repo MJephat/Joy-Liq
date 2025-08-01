886199c51c8c7f2abcf1d6010d10365bc8370ea5
jephat mukundi <mukundijephat22@gmail.com>
2025-08-01 16:24:14 +0300

    endpoints 2

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://shop-zcj5.onrender.com/"], 
+  origin: "https://shop-zcj5.onrender.com",
   credentials: true,
 }));
 
